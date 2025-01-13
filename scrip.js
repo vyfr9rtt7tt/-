2559f1ad34728c349f2d28c8a79057a25de1e46c
vyfr9rtt7tt <bohodirovbehruz201behruz@gmail.com>
2025-01-13 13:36:34 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,115 @@
+//Функцы js
+
+// for (let i = 0; i < array.length; i++) {
+    
+    
+// }
+
+// let count = 1;
+// // ЭТО КЛУЧЕВОЕ СЛОВО ДЛЯ ОПРЕДЕЛЕНИЕ ФУНКЦИИ
+// // ( ) -   ПРИНИМАЕТ ПАРАМЕТРЫ  /    { } - ТЕЛО ФУНКЦИИ
+
+
+// function HALP(params) {
+//     console.log('HALP');
+//     if (name) {
+//         console.log('Hello,{name}');
+        
+//     }else{
+//         console.log('Hello,world');
+// }
+// HALP();
+// ///  ПРИМЕР ФУНКЦИИ С АРГУМЕНТАМИ
+// function HALP(NAME) {
+//     console.log('HALP${name}');
+//     console.log(NAME);
+    
+// }
+// HALP('Behruz');      
+// function 12(behruz) {
+//     console.log('12');
+//     console.log(behruz);
+    
+// }   
+// 12('Behruz');
+// function 12(behruz) {
+//     console.log('12');
+//     console.log(behruz);
+    
+// }
+// console.log(12); 
+// Было:
+// Стало:
+// (пусто или другая логика)
+
+// Cтарый синтаксис
+// let Number = 0;
+
+switch (Number) {
+    case 1:
+    case 1:
+        console.log('1');
+
+        break;
+    case 2:
+
+    default:
+    console.log('default');
+    
+        break;
+}
+if (Number == 1) {
+    console.log('1');
+}else if (Number == 2) {
+    console.log('2');   
+}
+let name = 'Behruz';
+let age = 18;
+let isStudent = true;
+let isTeacher = false;
+resuilt = name + age + isStudent + isTeacher;
+console.log(resuilt);
+/////
+
+
+// for (let i = 0; i < 1000; i++) {
+//     if (array[i] == 1000) {
+//         console.log('1');
+
+
+
+    
+// }else if (array[i] == 2) 
+//     console.log('2'); }
+// Проверьте, что массив объявлен и содержит нужные данные
+let array = [/* здесь ваш массив */];
+
+for (let i = 0; i < 1000; i++) {
+    if (array[i] == 1000) {
+        console.log('1');
+    } else if (array[i] == 2) {
+        console.log('2');
+        
+    }
+}
+////цыклы
+
+// for (let i = 0; i < 1000; i++) {
+//     if (array[i] == 1000) {  
+//         console.log('1');
+
+//     } else if (array[i] == 2) {
+
+//         console.log('2');
+//     }
+// }
+// let array = [/* здесь ваш массив */];
+
+
+
+
+
+
+// let array = [/* здесь ваш массив */];
+
+ 
